bfc1694226dc919d6bf76577fc3d04884d55fef1
Sadman Kabir Soumik <sadmanks@gmail.com>
2020-07-15 02:11:28 +0600

    train valid

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pandas as pd
 import torch.nn as nn
 import pretrainedmodels
 from torch.nn import functional as F
+import albumentations
+from wtfml.data_loaders.image import ClassificationLoader
+
 
 # model
 class SEResNext(nn.Module):
@@ -22,6 +25,7 @@ class SEResNext(nn.Module):
         out = self.out(x)
         return out
 
+
 def run(fold):
     # train image path
     training_data_path = input("Enter the training path: ")
@@ -32,9 +36,40 @@ def run(fold):
     epochs = 30
     train_batch_size = 32
     valid_batch_size = 16
-    
+    mean = (0.485, 0.456, 0.225)
+    standard_deviation = (0.229, 0.224, 0.225)
+
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+    # normalize images
+    train_aug = albumentations.Compose([
+        albumentations.Normalize(mean,
+                                 std=standard_deviation,
+                                 max_pixel_value=255.0,
+                                 always_apply=True)
+    ])
+
+    valid_aug = albumentations.Compose([
+        albumentations.Normalize(mean,
+                                 std=standard_deviation,
+                                 max_pixel_value=255.0,
+                                 always_apply=True)
+    ])
+
+    # train image mapping
+    train_images = df_train.image_name.values.tolist()
+    train_images = [
+        os.path.join(training_data_path, i + ".jpg") for i in train_images
+    ]
+    train_targets = df_train.target.values
 
 
+    valid_images = df_train.image_name.values.tolist()
+    valid_images = [
+        os.path.join(training_data_path, i + ".jpg") for i in valid_images
+    ]
+    train_targets = df_valid.target.values
 
 
 
